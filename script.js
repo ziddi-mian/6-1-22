28049ad786eee1fba2953be599495aabfaf256ca
Ziddi-Mian <ahmadyounas893@gmail.com>
2022-01-06 12:42:37 +0500

    js

--- script.js
+++ script.js
@@ -33,3 +33,68 @@ function concate() {
     console.log("After concating:", concate);
   }
   concate();
+
+   // program 4
+// Write a js program to compare two strings.
+function compare() {
+    var string1 = "usama azam";
+    var string2 = "usama azam";
+    var length1 = string1.length;
+    var length2 = string2.length;
+    var check = 0;
+    if (length1 === length2) {
+      for (let x = 0; x < length1; x++) {
+        if (string1.charAt(x) == string2.charAt(x)) {
+          check++;
+        }
+      }
+    }
+    if (check == length1) {
+      console.log("Strings Are Equal");
+    } else {
+      console.log("Strings Are Not Equal");
+    }
+  }
+  compare();
+
+
+  // program 5
+// Write a js program to convert lowercase string to uppercase.
+function uppercase() {
+    var string1 = "usamA azam";
+    var upper = string1.toUpperCase();
+    console.log("After Conversion Into Uppercase: ", upper);
+  }
+  uppercase();
+
+
+   // program 6
+// Write a js program to convert uppercase string to lowercase.
+function lowercase() {
+    var string1 = "Muhammad UsaMA";
+    var lower = string1.toLowerCase();
+    console.log("After Conversion Into Uppercase: ", lower);
+  }
+  lowercase();
+
+
+   // program 7
+// Write a js program to toggle case of each character of a string.
+function togalcas() {
+    var string1 = "Muhammad UsaMA";
+    var alpha = "";
+    var z = "";
+    var togal;
+    for (let x = 0; x < string1.length; x++) {
+      alpha = string1.charAt(x);
+      if (alpha >= "a") {
+        var y = alpha.toUpperCase();
+        z = z.concat(y);
+      } else {
+        var y = alpha.toLowerCase();
+        z = z.concat(y);
+      }
+    }
+    console.log("After Togal: ", z);
+  }
+  togalcas();
